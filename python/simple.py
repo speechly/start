d14896d543de915263fb26d8b53968dc242998b0
Hannes Heikinheimo <hannes@speechgrinder.com>
2019-06-28 10:55:45 +0300

    simple examples for python and node
    licence file

---
+++ python/simple.py
@@ -0,0 +1,39 @@
+import os
+import wave
+
+import grpc
+
+from sg_pb2 import SluRequest, SluConfig, SluEvent, LoginRequest
+from sg_pb2_grpc import IdentityStub as IdentityService
+from sg_pb2_grpc import SluStub as SLUService
+
+chunk_size = 8000
+
+if 'APP_ID' not in os.environ:
+    raise RuntimeError('APP_ID environment variable needs to be set')
+
+
+def audio_iterator():
+    yield SluRequest(config=SluConfig(channels=1, sample_rate_hertz=16000))
+    yield SluRequest(event=SluEvent(event='START'))
+    with wave.open('../audio.wav', mode='r') as audio_file:
+        audio_bytes = audio_file.readframes(chunk_size)
+        while audio_bytes:
+            yield SluRequest(audio=audio_bytes)
+            audio_bytes = audio_file.readframes(chunk_size)
+    yield SluRequest(event=SluEvent(event='STOP'))
+
+
+with grpc.secure_channel('api.speechgrinder.com', grpc.ssl_channel_credentials()) as channel:
+    token = IdentityService(channel)\
+        .Login(LoginRequest(device_id='python-simple-test', app_id=os.environ['APP_ID']))\
+        .token
+
+with grpc.secure_channel('api.speechgrinder.com', grpc.ssl_channel_credentials()) as channel:
+    slu = SLUService(channel)
+    responses = slu.Stream(
+        audio_iterator(),
+        None,
+        [('authorization', 'Bearer {}'.format(token))])
+    for response in responses:
+        print(response)
